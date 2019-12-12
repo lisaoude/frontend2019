31982933acc7b66001c6218cd2bbee6c3cfbef83
Lisa Oude Elferink <57795294+lisaoude@users.noreply.github.com>
2019-12-12 22:08:20 +0100

    Add files via upload

---
+++ week5/chilly.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const line = document.querySelector('.line');
+const navigation = document.querySelector('.navigation');
+
+
+hamburger.addEventListener('click', animateHamburger);
+
+function animateHamburger() {
+    hamburger.classList.toggle('active');
+    navigation.classList.toggle('active');
+};
+
+console.log('dit werkt');
